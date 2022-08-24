efd5d22b21b03cca1323c29c4ef0490a0c2cac6e
unknown <cabaraul@gmail.com>
2022-08-24 02:21:20 -0400

    adding fixin errors

--- rest-rant/controllers/places.js
+++ rest-rant/controllers/places.js
@@ -1,26 +1,39 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const db = require('../models')
 
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
-
-
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
+  db.Place.find()
+  .then(places => {
+    res.render('places/index', { places })
   })
   .catch(err => {
-      console.log('err', err)
+    console.log(err) 
+    res.render('error404')
+  })
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    
+  db.Place.create(req.body)
+  .then(() => {
+    res.redirect('/places')
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+        message += `${field} was ${err.errors[field].value}`
+        message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+    } else {
       res.render('error404')
+    }
   })
 })
 
@@ -29,16 +42,15 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
-    .then(place => {
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', { place })
+  })
+  .catch( err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
-
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
